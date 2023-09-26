35fea7b5b396f6ec2813e2abf400797b52cc10da
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-09-26 11:11:46 -0400

    Update Server.java

--- src/fileClient/Server.java
+++ src/fileClient/Server.java
@@ -3,45 +3,36 @@ package fileClient;
 import java.io.*;
 import java.net.*;
 
-
 /*	TSI 3713 Sistemas Distribuídos
-    Professor: 
-	Rodrigo Andrade Cardoso
-	
-	Alunos:
-	Luiz Ferreira Neto
-  	Pablo Ruan Dias
- 	Marcos Alexandre da Silva
- 
- */
+Professor: 
+Rodrigo Andrade Cardoso
 
+Alunos:
+Luiz Ferreira Neto
+	Pablo Ruan Dias
+	Marcos Alexandre da Silva
+
+*/
 
 /**
- * Esta classe representa um servidor que atende a solicitações de clientes para listar arquivos (INDEX)
- * ou baixar arquivos (GET) de uma pasta compartilhada por meio de uma conexão de soquete.
+ * Esta classe representa o servidor para o sistema de gerenciamento de arquivos.
  */
 public class Server {
-    /**
-     * O método principal da classe que inicia o servidor e lida com as solicitações dos clientes.
-     *
-     * @param args Os argumentos da linha de comando (não são usados neste caso).
-     * @throws IOException Se ocorrer um erro de E/S durante a comunicação com o cliente.
-     */
     public static void main(String[] args) throws IOException {
-        // Configuração do servidor
-        int serverPort = 8080; // Porta em que o servidor está escutando
-        String directoryPath = "C:\\Users\\marco\\eclipse-workspace\\TrabalhoDois\\src\\arquivos_compartilhados"; // Caminho para a pasta de arquivos compartilhados
+        int serverPort = 8080; // Porta do servidor
+        String directoryPath = "C:\\Users\\marco\\eclipse-workspace\\TrabalhoDois\\src\\arquivos_compartilhados"; // Diretório de arquivos compartilhados
 
-        // Criação do soquete do servidor
+        // Cria um socket de servidor na porta especificada
         ServerSocket serverSocket = new ServerSocket(serverPort);
-        System.out.println("Servidor iniciado com a pasta no caminho: " + directoryPath + "\n Servidor disponível na porta: " + serverPort);
+        System.out.println(
+            "Server iniciado com o diretório: " + directoryPath + "\nServidor disponível na porta: " + serverPort);
 
         while (true) {
-            // Aceita uma conexão de cliente
+            // Aceita a conexão de um novo cliente
             Socket clientSocket = serverSocket.accept();
             System.out.println("Novo cliente conectado");
 
-            // Inicia uma nova thread para lidar com a comunicação com o cliente
+            // Cria uma nova thread para lidar com o cliente
             Thread clientThread = new Thread(() -> {
                 try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
@@ -49,36 +40,49 @@ public class Server {
                     String command;
                     while ((command = in.readLine()) != null) {
                         if (command.equals("INDEX")) {
-                            // Comando INDEX: Envia a lista de arquivos da pasta compartilhada para o cliente
+                            // Comando para listar arquivos no diretório
                             File directory = new File(directoryPath);
                             String[] fileList = directory.list();
                             String fileListString = String.join(", ", fileList);
                             out.println(fileListString);
                         } else if (command.startsWith("GET ")) {
-                            // Comando GET: Envia o conteúdo de um arquivo solicitado pelo cliente
+                            // Comando para enviar um arquivo ao cliente
                             String fileName = command.substring(4);
-                            File file = new File(directoryPath + "/" + fileName);
+                            File file = new File(directoryPath + File.separator + fileName);
+
                             if (file.exists() && file.isFile()) {
-                                out.println("OK"); // Confirma que o arquivo existe
-                                try (BufferedReader fileReader = new BufferedReader(new FileReader(file))) {
-                                    String line;
-                                    while ((line = fileReader.readLine()) != null) {
-                                        out.println(line); // Envia cada linha do arquivo ao cliente
+                                // Se o arquivo existe, lê e envia seu conteúdo para o cliente
+                                try (BufferedInputStream fileInputStream = new BufferedInputStream(
+                                        new FileInputStream(file))) {
+                                    byte[] buffer = new byte[1024];
+                                    int bytesRead;
+
+                                    out.println("OK");
+
+                                    try (BufferedOutputStream outputStream = new BufferedOutputStream(
+                                            clientSocket.getOutputStream())) {
+                                        while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                                            outputStream.write(buffer, 0, bytesRead);
+                                        }
                                     }
                                 }
                             } else {
-                                out.println("Arquivo não encontrado"); // Informa que o arquivo não foi encontrado
+                                // Se o arquivo não existe, envia um erro para o cliente
+                                out.println("ERROR Arquivo não encontrado");
                             }
+                            out.println("END_OF_FILE");
                         } else {
-                            out.println("Comando inválido"); // Responde se o comando não for reconhecido
+                            // Comando inválido
+                            out.println("Comando inválido");
                         }
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 } finally {
                     try {
+                        // Fecha a conexão com o cliente
                         clientSocket.close();
-                        System.out.println("Cliente desconectado");
+                        System.out.println("Client disconnected");
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
