447a5e749e60e1d8ecfc835a82206c59039a33b8
Marcos <marcosxx1.ma@gmail.com>
2023-09-27 08:33:57 -0400

    refactor: Implementa receber o caminho do cliente, ao invés de hardcoded.

--- src/fileClient/Server.java
+++ src/fileClient/Server.java
@@ -2,6 +2,10 @@ package fileClient;
 
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 /*	TSI 3713 Sistemas Distribuídos
 Professor: 
@@ -13,15 +17,32 @@ Luiz Ferreira Neto
 	Marcos Alexandre da Silva
 
 */
-
+ 
 /**
  * Esta classe representa o servidor para o sistema de gerenciamento de arquivos.
  */
 public class Server {
     public static void main(String[] args) throws IOException {
+    	Scanner entrada = new Scanner(System.in);
         int serverPort = 8080; // Porta do servidor
-        String directoryPath = "C:\\Users\\marco\\eclipse-workspace\\TrabalhoDois\\src\\arquivos_compartilhados"; // Diretório de arquivos compartilhados
 
+        String  directoryPath = "";
+        boolean validPath = false;
+
+        while (!validPath) {
+            System.out.println("Digite o diretório onde os arquivos estão localizados:");
+            directoryPath = entrada.nextLine();
+
+            // Check if the directory exists
+            Path path = Paths.get(directoryPath);
+            if (Files.isDirectory(path)) {
+                validPath = true;
+            } else {
+                System.out.println("Diretório inválido. Tente novamente.");
+            }
+        }
+
+        
         // Cria um socket de servidor na porta especificada
         ServerSocket serverSocket = new ServerSocket(serverPort);
         System.out.println(
@@ -31,6 +52,7 @@ public class Server {
             // Aceita a conexão de um novo cliente
             Socket clientSocket = serverSocket.accept();
             System.out.println("Novo cliente conectado");
+            final String finalDirectoryPath = directoryPath; 
 
             // Cria uma nova thread para lidar com o cliente
             Thread clientThread = new Thread(() -> {
@@ -38,44 +60,65 @@ public class Server {
                      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
                     String command;
-                    while ((command = in.readLine()) != null) {
-                        if (command.equals("INDEX")) {
-                            // Comando para listar arquivos no diretório
-                            File directory = new File(directoryPath);
-                            String[] fileList = directory.list();
-                            String fileListString = String.join(", ", fileList);
-                            out.println(fileListString);
-                        } else if (command.startsWith("GET ")) {
-                            // Comando para enviar um arquivo ao cliente
-                            String fileName = command.substring(4);
-                            File file = new File(directoryPath + File.separator + fileName);
-
-                            if (file.exists() && file.isFile()) {
-                                // Se o arquivo existe, lê e envia seu conteúdo para o cliente
-                                try (BufferedInputStream fileInputStream = new BufferedInputStream(
-                                        new FileInputStream(file))) {
-                                    byte[] buffer = new byte[1024];
-                                    int bytesRead;
-
-                                    out.println("OK");
-
-                                    try (BufferedOutputStream outputStream = new BufferedOutputStream(
-                                            clientSocket.getOutputStream())) {
-                                        while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                                            outputStream.write(buffer, 0, bytesRead);
-                                        }
-                                    }
-                                }
-                            } else {
-                                // Se o arquivo não existe, envia um erro para o cliente
-                                out.println("ERROR Arquivo não encontrado");
+                    while (true) {
+                        try {
+                            command = in.readLine();
+                            if (command == null) {
+                                // O cliente fechou a conexão, paramos o loop
+                                break;
                             }
-                            out.println("END_OF_FILE");
-                        } else {
-                            // Comando inválido
-                            out.println("Comando inválido");
+
+                            if (command.equals("INDEX")) {
+                                // Comando para listar arquivos no diretório
+                                File directory = new File(finalDirectoryPath);
+                                String[] fileList = directory.list();
+                                String fileListString = String.join(", ", fileList);
+                                out.println(fileListString);
+                            } else if (command.startsWith("GET ")) {
+                                // Comando para enviar um arquivo ao cliente
+                                String fileName = command.substring(4);
+                                File file = new File(finalDirectoryPath + File.separator + fileName);
+
+                                if (file.exists() && file.isFile()) {
+                                    // Se o arquivo existe, lê e envia seu conteúdo para o cliente
+                                    FileInputStream fileInputStream = new FileInputStream(file);
+                                    try (BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)) {
+                                        byte[] buffer = new byte[1024];
+                                        int bytesRead;
+
+                                        out.println("OK");
+
+                                        try (BufferedOutputStream outputStream = new BufferedOutputStream(
+                                                clientSocket.getOutputStream())) {
+                                            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
+                                                outputStream.write(buffer, 0, bytesRead);
+                                            }
+                                        }
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    } finally {
+                                        // Fecha o arquivo após a transferência
+                                        fileInputStream.close();
+                                    }
+                                } else {
+                                    // Se o arquivo não existe, envia um erro para o cliente
+                                    out.println("ERROR Arquivo não encontrado");
+                                }
+                                out.println("END_OF_FILE");
+                            } else {
+                                // Comando inválido
+                                out.println("Comando inválido");
+                            }
+                        } catch (SocketException se) {
+                            // Lida com SocketException com a desconexão do cliente
+                            System.out.println("Client disconnected");
+                            break; // Para o loop para sair da thread
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            break; // Para o loop para sair da thread em caso de outras exceções
                         }
-                    }
+                    } 
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 } finally {
